53dba873a3f6e8fb6c67045d2d41ff82c18fec60
Yang Liu <yaliu@MLLA3853.magicleap.ds>
2022-04-08 11:10:25 -0400

    update models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name + "," + sel.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +25,43 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    truck = "truck"
+    sedan = "sedan"
+    suv = "suv"
+    type_choices = [
+        (truck, "Truck"),
+        (sedan, "Sedan"),
+        (suv, "SUV"),
+    ]
+    carmodel = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.TextField()
+    type = models.CharField(max_length=15, choices=type_choices)
+    year = models.DateField()
+
+    def __str__(self):
+        return str(self.year) + "/" + self.name + "/" + self.type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        vars(self).update(locals())
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(
+        self,
+        dealership,
+        name,
+        purchase,
+        review,
+        purchase_date,
+        car_make,
+        car_model,
+        car_year,
+        id,
+    ):
+        vars(self).update(locals())
